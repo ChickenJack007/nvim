1a61b7bd676757bdb30a860ff7417c6fb538dbd6
ChickenJack007 <j1melson@outlook.com>
2025-12-24 10:45:37 -0600

    Add files via upload

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,8 +1,13 @@
 vim.opt.number = true
 --vim.opt.cursorline = true
 vim.opt.relativenumber = true
-vim.opt.shiftwidth = 4
+vim.opt.shiftwidth = 2
+vim.o.foldmethod = "indent"
+vim.o.foldenable = true
+vim.o.foldlevel = 1
 
 vim.g.mapleader = " "
 -- vim.keymap.set("n", "<leader>cd", vim.cmd.Ex) --not needed if using snacks.nvim
 vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = false })
+vim.keymap.set("n", "f", "za", opts)
+
