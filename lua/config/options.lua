a960be299bc7b9c4855b3430a9f5af903069fd44
Jack Melson <j1melson@outlook.com>
2026-03-18 08:45:58 -0500

    disabled wrap and changed foldlevel

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,9 +4,10 @@ vim.opt.relativenumber = true
 vim.opt.shiftwidth = 2
 vim.o.foldmethod = "indent"
 vim.o.foldenable = true
-vim.o.foldlevel = 1
+vim.o.foldlevel = 99
 
 vim.g.mapleader = " "
+vim.opt.wrap = false
 -- vim.keymap.set("n", "<leader>cd", vim.cmd.Ex) --not needed if using snacks.nvim
 vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = false })
 vim.keymap.set("n", "z", "za", opts)
